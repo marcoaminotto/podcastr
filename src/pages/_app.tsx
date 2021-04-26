0b174c74eb3ea6a17b2dcf20a7cb74ec58eccd03
Marco Echevestre <marcoaminotto@hotmail.com>
2021-04-26 16:00:28 +0200

    Fix mistake in code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,8 +9,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <div className={styles.wrapper}>
       <main>
-        <Header />;
-        <Component {...pageProps} />;
+        <Header />
+        <Component {...pageProps} />
       </main>
       <Player />
     </div>
