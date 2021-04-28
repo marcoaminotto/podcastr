ece9cfbcb2cb1b783283179150f4820c6a56eefa
Marco Echevestre <marcoaminotto@hotmail.com>
2021-04-28 11:40:42 +0200

    Fix error at returning values

--- src/utils/convertDurationToTimeString.ts
+++ src/utils/convertDurationToTimeString.ts
@@ -3,9 +3,10 @@ export function convertDurationToTimeString(duration: number) {
   const minutes = Math.floor((duration % 3600) / 60);
   const seconds = duration % 60;
 
-  const timeString = [hours, minutes, seconds].map((unit) => {
-    String(unit).padStart(2, '0');
-  }).join(':');
-
+  const timeString = [hours, minutes, seconds]
+    .map((unit) => {
+      return String(unit).padStart(2, '0');
+    })
+    .join(':');
   return timeString;
 }
