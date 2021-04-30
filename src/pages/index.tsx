33ea962668a1e7785e392acd1bcfdf3fa9ade5a2
Marco Echevestre <marcoaminotto@hotmail.com>
2021-04-30 20:40:19 +0200

    Implement audio feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,7 +54,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Play episode" />
                 </button>
               </li>
