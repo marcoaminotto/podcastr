a242c1c0eb85097a9758a41c4ff58f157d638822
Marco Echevestre <marcoaminotto@hotmail.com>
2021-04-30 21:16:28 +0200

    organize types at PlayerContext

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, ReactNode } from 'react';
 
 type Episode = {
   title: string;
@@ -17,14 +17,20 @@ type PlayerContextData = {
   setPlayingState: (state: boolean) => void;
 };
 
+type PlayerContextProviderProps = {
+  children: ReactNode;
+};
+
 export const PlayerContext = createContext({} as PlayerContextData);
 
-export function PlayerContextProvider({ children }) {
+export function PlayerContextProvider({
+  children,
+}: PlayerContextProviderProps) {
   const [episodeList, setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
 
-  function play(episode: object) {
+  function play(episode: Episode) {
     setEpisodeList([episode]);
     setCurrentEpisodeIndex(0);
     setIsPlaying(true);
